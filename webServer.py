f2184bb3db91bf9f68ce9b0ae4940f87542beedd
jcart657 <jc12690@nyu.edu>
2024-02-01 16:59:25 -0500

    Removed "Server-" from Connection statements in all headers.

--- webServer.py
+++ webServer.py
@@ -33,7 +33,7 @@ def webServer(port=13331):
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
       #Content-Type is an example on how to send a header as bytes. There are more!
       #outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
-      header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nServer-Connection: close\r\n' #+ outputdata
+      header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: close\r\n' #+ outputdata
       connectionSocket.send(header.encode())
 
       for i in f: #for line in file
@@ -45,7 +45,7 @@ def webServer(port=13331):
     except Exception as e:
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
-      header = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nServer-Connection: keep-alive\r\n' #+ outputdata
+      header = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: keep-alive\r\n' #+ outputdata
       connectionSocket.send(header.encode())
 
       #Close client socket
