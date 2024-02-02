7a94897c357480f0ee64858e30296f2e95a648a5
jcart657 <jc12690@nyu.edu>
2024-02-02 15:33:09 -0500

    Moved the f.close() statement to attempt to get HTML content to show in the message.

--- webServer.py
+++ webServer.py
@@ -1,4 +1,5 @@
 # import socket module
+import socket
 from socket import *
 # In order to terminate the program
 import sys
@@ -16,19 +17,20 @@ def webServer(port=13331):
 
   while True:
     #Establish the connection
+    #host = serverSocket.getsockname()
+    #print(host)
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
+
 
     try:
       # Client sends a message containing the file
       message = connectionSocket.recv(1024).decode()
       filename = message.split()[1]
-
       #opens the client requested file.
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       f = open(filename[1:], 'rb')
       outputdata = f.read()
-      f.close()
 
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
       #Content-Type is an example on how to send a header as bytes. There are more!
@@ -50,9 +52,10 @@ def webServer(port=13331):
 
       #Close client socket
       connectionSocket.close()
-      # Next, close the server socket, right?
+      # Next, close the file and server socket, right?
 
   #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+  f.close()
   serverSocket.close()
   sys.exit()  # Terminate the program after sending the corresponding data
 
