f50df74f38bd50dee250fe433b10bf9a826e1f29
jcart657 <jc12690@nyu.edu>
2024-02-14 14:54:26 -0500

    Attempt to see body content in HTML message.

--- webServer.py
+++ webServer.py
@@ -26,7 +26,7 @@ def webServer(port=13331):
       filename = message.split()[1]
       #opens the client requested file.
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], 'rb')
+      f = open(filename[1:], 'r')
       #f.read()
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
       #Content-Type is an example on how to send a header as bytes. There are more!
@@ -41,7 +41,7 @@ def webServer(port=13331):
 
       for i in f: #for line in file
       #Send the content of the requested file to the client (don't forget the headers you created)!
-        header += i
+        header += i.encode()
         connectionSocket.sendall(header)
         connectionSocket.close() #closing the connection socket
         f.close()
