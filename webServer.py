0ae9b2cf144b2483f246865f361a403f800532c2
jcart657 <jc12690@nyu.edu>
2024-02-14 12:03:10 -0500

    Attempt to see body content in HTML message.

--- webServer.py
+++ webServer.py
@@ -27,18 +27,16 @@ def webServer(port=13331):
       #opens the client requested file.
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       f = open(filename[1:], 'rb')
-      outputdata = f.read()
-      f.close()
-
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
       #Content-Type is an example on how to send a header as bytes. There are more!
       #outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
       header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: close\r\nServer: CarterPortnoyPythonServer/2.1.2024\r\n' #+ outputdata
       connectionSocket.send(header.encode())
+      f.close()
 
       for i in f: #for line in file
       #Send the content of the requested file to the client (don't forget the headers you created)!
-        outputdata += i
+        header += i
         connectionSocket.send(outputdata.encode())
 
         connectionSocket.close() #closing the connection socket
@@ -46,8 +44,8 @@ def webServer(port=13331):
     except Exception as e:
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
-      header = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: keep-alive\r\nServer: CarterPortnoyPythonServer/2.1.2024\r\n' #+ outputdata
-      connectionSocket.send(header.encode())
+      oopsHeader = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: keep-alive\r\nServer: CarterPortnoyPythonServer/2.1.2024\r\n' #+ outputdata
+      connectionSocket.send(oopsHeader.encode())
 
       #Close client socket
       connectionSocket.close()
