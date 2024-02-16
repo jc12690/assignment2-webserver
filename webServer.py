e8b7ed6cc107667e56d89737ae6ea3369d3f0d53
jcart657 <jc12690@nyu.edu>
2024-02-16 14:35:20 -0500

    Started from scratch.

--- webServer.py
+++ webServer.py
@@ -1,5 +1,4 @@
 # import socket module
-import socket
 from socket import *
 # In order to terminate the program
 import sys
@@ -7,51 +6,68 @@ import sys
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
 
-  #Prepare a server socket
-  serverSocket.bind(("127.0.0.1", port))
-
-  # The server listens on the specified port
+  # Prepare a server socket
+  serverSocket.bind(("", port))
   serverSocket.listen(1)
+  # Fill in start
+
+  # Fill in end
 
   while True:
-    #Establish the connection
+    # Establish the connection
+
     print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept() # Fill in start -are you accepting connections?     #Fill in end
 
     try:
-      # Client sends a message containing the file
-      message = connectionSocket.recv(1024)
+      message =  connectionSocket.recv(1024) # Fill in start -a client is sending you a message   #Fill in end
       filename = message.split()[1]
-      #opens the client requested file.
-      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], 'rb')
-      #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
-      #Content-Type is an example on how to send a header as bytes. There are more!
-      header = 'HTTP/1.1 200 OK\r\n'
-      header += 'Content-Type: text/html; charset=UTF-8\r\n'
-      header += 'Server: CarterPortnoyPythonServer/2.1.2024\r\n'
-      header += 'Connection: keep-alive\r\n\r\n'
 
-      for i in f: #for line in file
-      #Send the content of the requested file to the client (don't forget the headers you created)!
-        header += i
-        f.close()
-        connectionSocket.send(header)
+      # opens the client requested file.
+      # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
+      f = open(filename[1:], "rb") # fill in start #fill in end)
+      # fill in end
+
+      # This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
+      # Fill in start
+
+      # Content-Type is an example on how to send a header as bytes. There are more!
+      outputdata = "HTTP/1.1 200 OK\r\n"
+      outputdata += "Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += "Connection: close\r\n"
+      outputdata += "Server: CarterPortnoy\r\n\r\n"
+
+    # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+
+    # Fill in end
+
+      for i in f:
+        outputdata += i
+        connectionSocket.send(outputdata)
+
+        connectionSocket.close()  # closing the connection socket
 
     except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      # Remember the format you used in the try: block!
-      notfound = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: keep-alive\r\nServer: CarterPortnoyPythonServer/2.1.2024\r\n\r\n'
-      connectionSocket.send(notfound)
+      oops = "HTTP/1.1 404 Not Found\r\n"
+      oops += "Content-Type: text/html; charset=UTF-8\r\n"
+      oops += "Connection: close\r\n\r\n"
+      connectionSocket.send(oops)
 
-      #Close client socket
-      connectionSocket.close()
-      # Next, close the file and server socket, right?
+# Send response message for invalid request due to the file not being found (404)
+# Remember the format you used in the try: block!
+# Fill in start
 
-  #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
-  #serverSocket.close()
-  #f.close()
-  #sys.exit()  # Terminate the program after sending the corresponding data
+# Fill in end
+
+# Close client socket
+# Fill in start
+
+# Fill in end
+
+# Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+# serverSocket.close()
+# sys.exit()  # Terminate the program after sending the corresponding data
+
 
 if __name__ == "__main__":
   webServer(13331)
